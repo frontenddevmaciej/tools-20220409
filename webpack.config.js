4413d2516f6faa5f5336507b9731008a1e85a7ff
frontenddevmaciej <frontenddevmaciej@gmail.com>
2022-04-09 14:52:14 +0200

    Prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ['./src/main.js', './src/style.scss'],
+  entry: ["./src/main.js", "./src/style.scss"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html'
+      template: "./src/index.html",
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
   ],
   module: {
-    rules: [{
-      test: /.scss$/,
-      use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
-    }
-    ]
-  }
+    rules: [
+      {
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
