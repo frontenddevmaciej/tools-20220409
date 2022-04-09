287442103162cc1db2bddeaa8a1e12cdcacc2af6
frontenddevmaciej <frontenddevmaciej@gmail.com>
2022-04-09 12:09:13 +0200

    CSS stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: ['./src/main.js', './src/style.css'],
@@ -10,12 +11,13 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [{
       test: /.css$/,
-      use: ['style-loader','css-loader']
+      use: [MiniCssExtractPlugin.loader,'css-loader']
     }
     ]
   }
